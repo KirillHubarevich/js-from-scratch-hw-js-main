9d313e3883b37f5402cc5cc07b98ad843842543b
KirillHubarevich <kgubarevich@inbox.ru>
2025-09-27 13:19:45 +0300

    Commitiwe

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,21 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+const n = 1
+const x = 2
+let even = true
+let b
+
+function isEven(a) {
+    b = a%(x)
+    if (b === n)
+       even = false
+    else even = true
+    return even}
+
+isEven(15)
+console.log(even)
+
+
+// n = n% (n+i);
+// выражение 5 % 2 вернёт остаток 1.
