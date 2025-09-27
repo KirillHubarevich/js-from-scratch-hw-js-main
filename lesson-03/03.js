6901f69d0a2da2491e5700c5e47126c7f0e64475
KirillHubarevich <kgubarevich@inbox.ru>
2025-09-27 15:46:18 +0300

    CommitiweCom

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -3,14 +3,22 @@
 let largest
 
 function findLargest(a,b,c) {
-    if (a > b && a > c )
+    if (a > b && a > c)
         largest = a
     if (b > a && b > c)
         largest = b
     if (c > a && c > b)
         largest = c
-    return largest
-    }
-findLargest(700,400,1000)
+    else if (a === c && a === b)
+    largest = a || b || c
+    return largest }
+
+findLargest(22,22,23)
 console.log('Наибольшее число -', largest)
+
+
+
+
+
+
 
