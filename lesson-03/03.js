f178a08953580036415a1e7b69f6d169b63edd7f
KirillHubarevich <kgubarevich@inbox.ru>
2025-09-27 15:20:47 +0300

    CommitiweCom

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,16 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+let largest
+
+function findLargest(a,b,c) {
+    if (a > b && a > c )
+        largest = a
+    if (b > a && b > c)
+        largest = b
+    if (c > a && c > b)
+        largest = c
+    return largest
+    }
+findLargest(700,400,1000)
+console.log('Наибольшее число -', largest)
+
