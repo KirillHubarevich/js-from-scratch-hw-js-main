7089d7d449b55407594f8e309cd244baaf66cf44
KirillHubarevich <kgubarevich@inbox.ru>
2025-10-17 20:55:43 +0300

    06-01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,89 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNumbers = []
+const filter = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        const index = array.indexOf(element);
+        callback(element, index);
+    } return numbers
+}
+
+const oddNumbers = filter (numbers, (element, index) => {
+    return newNumbers.push (element < 10)
+});
+console.log(oddNumbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     if (book.price < 30) {
+//         console.log(`${book.title} : ${book.price}`);
+//     }
+// }
+
+// processBook(favoriteBooks, b => console.log (b.author));
+
+// const needNumbers = numbers.filter (number => number < 5)
+// console.log(needNumbers)
+
+
+
+// // b = book
+//     const printCheapBook = (book) => {
+//         if (book.price < 30) {
+//             console.log(`${book.title} : ${book.price}`);
+//         }
+//     }
+// // processBook(favoriteBooks, printCheapBook);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
