7c1ebf3ba5d91090b8a22486555a7cb0d3255179
KirillHubarevich <kgubarevich@inbox.ru>
2025-10-25 17:44:34 +0300

    lesson-07-01

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,56 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const NextButton = document.getElementById('next-button')
+const PrevButton = document.getElementById('prev-button')
+const image = document.getElementById('web-tech-image')
+
+
+
+
+NextButton.addEventListener('click', function () {
+    if (image.src === WEB_TECH_IMAGES[0]) {
+        image.src = WEB_TECH_IMAGES[1];
+    } else if (image.src === WEB_TECH_IMAGES[1]) {
+        image.src = WEB_TECH_IMAGES[2]
+    } else if (image.src === WEB_TECH_IMAGES[2]) {
+        image.src = WEB_TECH_IMAGES[0]
+    }
+});
+
+PrevButton.addEventListener('click', function () {
+    if (image.src === WEB_TECH_IMAGES[0]) {
+        image.src = WEB_TECH_IMAGES[2];
+    } else if (image.src === WEB_TECH_IMAGES[2]) {
+        image.src = WEB_TECH_IMAGES[1]
+    } else if (image.src === WEB_TECH_IMAGES[1]) {
+        image.src = WEB_TECH_IMAGES[0]
+    }
+});
+
+
+
+
+// Должно происходить: клик (next) - изображение [1] - стало [2], клик (next) - [3], клик (next) - стало [1] и по циклу
+// И должно происходить: клик (prev) - изображение [1] - стало [3], клик (prev) - [2], клик (prev) - стало [1] и по циклу
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
